a0fa7cd08780a1ac4d29d4e07b56a467b68eaca5
Huong Bui-Vu <huong.buivu@gmail.com>
2023-08-02 08:11:15 +0400

    raycasting continued

--- sources/raycasting.c
+++ sources/raycasting.c
@@ -26,7 +26,8 @@ void	raycasting(t_main *main)
 	double	inter_y
 	int		step_x;
 	int		step_y;
-
+	double	wall_dist;
+	
 	//set position of player in unit coordinates and map coordinates -> this would require conversion of everything else
 	// pl_x = main->grid_size * main->player_pos[0] + (main->grid_size / 2);
 	// pl_y = main->grid_size * main->player_pos[1] + (main->grid_size / 2);
@@ -39,6 +40,7 @@ void	raycasting(t_main *main)
 	pl_dirx = main->player_vec[0];
 	pl_diry = main->player_vec[1];
 
+	//for the camera plane, we are working only in units of 1
 	//set camera plane direction (only the direction, no magnitude, so it's in unit based on pl dir)
 	//we set it to .65 since we want a FOV of 66 degrees. tan(33) = camera_dirx / 1 
 	if (pl_dirx == 0)
@@ -50,7 +52,7 @@ void	raycasting(t_main *main)
 	else
 		camera_diry = 0;
 
-	//get the magnitude of the camera plane <- check this later
+	//get the magnitude of the camera plane <- still in units of 1
 	//we have to send out rays for every x on the screen so the magnitude will shorten/lengthen camera plane
 	int	x;
 	x = 1;
@@ -77,10 +79,51 @@ void	raycasting(t_main *main)
 		delta_y = 0;
 
 	//find the initial intersect at horizontal and vertical lines on map grid
+	//the intersect will always give you a whole number
+	//x indicates the column line that 
 	if (ray_x < 0) //going left
 	{
 		step_x = -1;
-		inter_x = (map_x - pl_x)
+		inter_x = (pl_x - map_x) + delta_x;
+	}
+	else //going right
+	{
+		step_x = 1;
+		inter_x = (map_x + 1 - pl_x) + delta_x;
+	}
+	if (ray_y < 0) // going up
+	{
+		step_y = -1;
+		inter_y = (pl_y - map_y) + delta_y;
+	}
+	else //going down
+	{
+		stepy_y = 1;
+		inter_y = (map_y + 1 - pl_y) + delta_y
 	}
 
+	//travel through ray until we hit a wall
+	while (/*we don't hit a wall*/)
+	{
+		if (inter_x < inter_y)
+		{
+			inter_x += delta_x;
+			map_x += step_x;
+			side = 0;
+		}
+		else 
+		{
+			inter_y += delta_y;
+			map_y += step_y;
+			side = 1;
+		}
+		if (main->map[map_x][map_y] == 1)
+			//we hit a wall
+	}
+
+	//calculate perpendicular distance from camera plane to wall
+	if (side = 0)
+		wall_dist = inter_x - delta_x;
+	else
+		wall_dist = inter_y - delta_y
 }
