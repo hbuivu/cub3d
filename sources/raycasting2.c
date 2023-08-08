5fcfef7d123b4de0f883beeaca082fefd6f6fd75
Huong Bui-Vu <huong.buivu@gmail.com>
2023-08-08 13:10:02 +0400

    success??

--- sources/raycasting2.c
+++ sources/raycasting2.c
@@ -28,7 +28,7 @@ void	calc_step(t_main *main)
 		main->calc->stepx = 1;
 	else if (main->calc->angle > main->n_angle && main->calc->angle < main->s_angle)
 		main->calc->stepx = -1;
-	if (float_is(main->calc->angle, 0) || float_is(main->calc->angle, M_PI))
+	if (float_is(main->calc->angle, 0) || float_is(main->calc->angle, M_PI) || float_is(main->calc->angle, main->e_angle))
 		main->calc->stepy = 0;
 	else if (main->calc->angle > 0 && main->calc->angle < M_PI)
 		main->calc->stepy = -1;
@@ -66,6 +66,8 @@ void	recalc(t_main *main)
 	main->calc->angle += main->calc->ray_incr;
 	if (main->calc->angle < 0)
 		main->calc->angle += 2 * M_PI;
+	else if (main->calc->angle > main->e_angle)
+		main->calc->angle -= main->e_angle;
 	calc_step(main);
 }
 
@@ -100,17 +102,23 @@ void	raycast(t_main *main)
 	
 	c = main->calc;
 	x = 0;
+
+	// (void)x;
+	// (void)c;
+
+	// print_calc(main);
+	// printf("t or f: %i\n", float_is(6.283185, main->e_angle));
 	
-
-
 	while (x < main->calc->pln_width)
 	{
-		printf("x: %i\n", x);
+		printf("\n");
+		printf("x is: %i\n", x);
 		printf("angle: %lf\n", main->calc->angle);
+		printf("angle_incr: %lf\n", main->calc->ray_incr);
 		//find deltax and deltay (use deltay with col, and deltax with row)
 		//the ray moves for deltay each time it jumps one column
 		//the ray moves for deltax each time it jumps one row
-		if (float_is(c->angle, 0) || float_is(c->angle, M_PI))
+		if (float_is(c->angle, 0) || float_is(c->angle, M_PI) || float_is(c->angle, main->e_angle))
 		{
 			c->deltay = 0;
 			c->deltax = 64;
@@ -151,17 +159,17 @@ void	raycast(t_main *main)
 				c->row_int = round_down(c->py / c->upg) * c->upg - 1;
 			c->row_intx = c->px + (c->stepx * fabs((c->row_int - c->py) / tan(c->angle)));
 		}
-		// if (x == 117)
-		// {
-		// 	printf("c->col_inty: %lf\n", c->col_inty);
-		// 	printf("c->col_int: %lf\n", c->col_int);
-		// }
-		// if (x == 117)
-		// {
-		// 	printf("117: check row: %i check column: %i\n", (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
-		// 	printf("mw: %i mh: %i\n", main->map_width, main->map_height);
-		// }
+		printf("***BEGINNING***\n");
+		printf("c->col_inty: %lf\n", c->col_inty);
+		printf("c->col_int: %lf\n", c->col_int);
+		printf("COL: %i: check row: %i check column: %i\n", x, (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
+		printf("c->row_int: %lf\n", c->row_int);
+		printf("c->row_intx: %lf\n", c->row_intx);
+		printf("ROW: %i: check row: %i check column: %i\n", x, (int)(c->row_int / c->upg), (int)(c->row_intx / c->upg));	
+		printf("deltay: %lf\n", c->deltay);
+		printf("deltax: %lf\n", c->deltax);
 		// check for a wall jumping columns
+		printf("\n***COLUMN JUMPS***\n");
 		while ((int)(c->col_inty / c->upg) < main->map_height && 
 			(int)(c->col_int / c->upg) < main->map_width &&
 			(main->map[(int)(c->col_inty / c->upg)][(int)(c->col_int / c->upg)] != '1'))
@@ -172,43 +180,56 @@ void	raycast(t_main *main)
 				c->col_inty = 0;
 			if (c->col_int < 0)
 				c->col_int = 0;
-			if (x == 117)
-			{
-				printf("deltay: %lf\n", c->deltay);
-				printf("c->col_inty: %lf\n", c->col_inty);
-				printf("c->col_int: %lf\n", c->col_int);
-				printf("418: check row: %i check column: %i\n", (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
-				printf("mw: %i mh: %i\n", main->map_width, main->map_height);
-			}
+		
+			printf("deltay: %lf\n", c->deltay);
+			printf("c->col_inty: %lf\n", c->col_inty);
+			printf("c->col_int: %lf\n", c->col_int);
+			printf("%i: check row: %i check column: %i\n", x, (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
 		}
 
 		//check for a wall jumping rows
+		printf("\n***ROW JUMPS***\n");
+		// if (x == 625)
+		// {
+		// 	printf("c at row: %i col: %i: %c\n", (int)(c->row_int / c->upg), (int)(c->row_intx / c->upg), main->map[(int)(c->row_int / c->upg)][(int)(c->row_intx / c->upg)]);
+		// }
 		while ((int)(c->row_int / c->upg) < main->map_height &&
 			(int)(c->row_intx / c->upg) < main->map_width &&
-			main->map[(int)(c->row_int / c->upg)][(int)(c->row_int / c->upg)] != '1')
+			main->map[(int)(c->row_int / c->upg)][(int)(c->row_intx / c->upg)] != '1')
 		{
 			c->row_int += c->stepy * c->upg;
 			c->row_intx += c->stepx * c->deltax;
+			printf("deltax: %lf\n", c->deltax);
+			printf("c->row_intx: %lf\n", c->row_intx);
+			printf("c->row_int: %lf\n", c->row_int);
+			printf("%i: check row: %i check column: %i\n", x, (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
 		}
 
 		//find the point that is the shortest distance from original px and py
 		c->dist_col = fabs((c->px - c->col_int) / cos(c->angle));
 		c->dist_row = fabs((c->px - c->row_intx) / cos(c->angle));
 
-		printf("check row: %i check column: %i\n", (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
-		printf("check row: %i check column: %i\n", (int)(c->row_int / c->upg), (int)(c->row_intx / c->upg));
-		printf("dist_col: %lf\n", c->dist_col);
-		printf("dist_row: %lf\n", c->dist_row);
-
 		//calculate the "corrected" distance to wall
 		if (c->dist_col <= c->dist_row)
-			c->cor_dist = c->dist_col * cos(c->fov / 2);
+		{
+			printf("\nUsing dist_col\n");
+			c->cor_dist = c->dist_col * cos((c->fov - (2 * x * c->ray_incr)) / 2);
+		}
 		else
-			c->cor_dist = c->dist_row * cos(c->fov / 2);
-
+		{
+			printf("\nUsing dist_row\n");
+			c->cor_dist = c->dist_row * cos((c->fov - (2 * x * c->ray_incr)) / 2);
+		}
+		
 		//calculate wall height at that point
 		// ratio of actual wall height / actual distance from wall  = projected wall height / player distance from projection plane
 		c->wall_height = (c->upg / c->cor_dist) * c->pln_dist;
+		printf("COL: check row: %i check column: %i\n", (int)(c->col_inty / c->upg), (int)(c->col_int / c->upg));
+		printf("ROW: check row: %i check column: %i\n", (int)(c->row_int / c->upg), (int)(c->row_intx / c->upg));
+		printf("dist_col: %lf\n", c->dist_col);
+		printf("dist_row: %lf\n", c->dist_row);
+		printf("cor_dist: %lf\n", c->cor_dist);
+		printf("wall_height: %lf\n", c->wall_height);
 
 		//draw the wall
 		draw_wall(x, main);
