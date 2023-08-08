cdaa556e45b3b92bafab9629f46a7e4d5483a5ef
Huong Bui-Vu <huong.buivu@gmail.com>
2023-08-08 22:38:16 +0400

    edited movement.c

--- sources/movement.c
+++ sources/movement.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   movement.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zsyyida <zsyyida@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*   By: hbui-vu <hbui-vu@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 20:51:30 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/08/08 21:02:09 by zsyyida          ###   ########.fr       */
+/*   Updated: 2023/08/09 01:37:40 by hbui-vu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,73 @@
 	ft_printf("keycode = %d\n", key_code);
  do not use else if or game stops
 */
+
+//NOTE: Check if this works w floats
+
 int	ft_movement(int key_code, t_main *main)
 {
+	int	i = -1;
+	
 	if (key_code == ESC)
 		ft_close(main);
-	// if (key_code == LEFT_KEY || key_code == A_KEY)
-
-	// if (key_code == RIGHT_KEY || key_code == D_KEY)
-
-	// if (key_code == UP_KEY || key_code == W_KEY)
-
-	// if (key_code == DOWN_KEY || key_code == S_KEY)
-
+	else if (key_code == LEFT_KEY)
+	{
+		main->calc->pdir -= 0.0349066; //2 degrees
+		if (main->calc->pdir < 0)
+			main->calc->pdir += main->e_angle;
+		main->calc->angle = main->calc->pdir - (main->calc->fov / 2);
+		if (main->calc->angle < 0)
+			main->calc->angle += 2 * M_PI;
+		calc_step(main);
+	}
+	else if (key_code == RIGHT_KEY)
+	{
+		main->calc->pdir += 0.0349066; //2 degrees
+		if (main->calc->pdir > main->e_angle)
+			main->calc->pdir -= main->e_angle;
+		main->calc->angle = main->calc->pdir - (main->calc->fov / 2);
+		if (main->calc->angle < 0)
+			main->calc->angle += 2 * M_PI;
+		calc_step(main);
+	}
+	else if (key_code == A_KEY)
+	{
+		while (--i < 5)
+		{
+			if (main->calc->px % 64 == 0 && main->map[(int)(main->calc->py / 64)][(int)((main->calc->px / 64) - 1)] == '1')
+				break;
+			main->calc->px--;
+		}
+	}
+	else if (key_code == D_KEY)
+	{
+		while (++i < 5)
+		{
+			if (main->map[(int)(main->calc->py / 64)][(int)((main->calc->px / 64))] == '1')
+				break;
+			main->calc->px++;
+		}
+	}
+	else if (key_code == W_KEY)
+	{
+		while (--i < 5)
+		{
+			if (main->calc->py % 64 == 0 && main->map[(int)((main->calc->py / 64) - 1)][(int)(main->calc->px / 64)] == '1')
+				break;
+			main->calc->py--;
+		}
+	}
+	else if (key_code == S_KEY)
+	{
+		while (++i < 5)
+		{
+			if (main->map[(int)(main->calc->py / 64)][(int)((main->calc->px / 64))] == '1')
+				break;
+			main->calc->py++;
+		}
+	}
+	mlx_clear_window(main->mlx->mlx_ptr, main->mlx->mlx_win);
+	mlx_destroy_image(main->mlx->mlx_ptr, main->img->img);
+	raycast(main);
     return (0);
 }
