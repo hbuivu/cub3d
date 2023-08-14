1e6795816ebd0e76aa1d38ba93b4ec6c2d6b2aba
Huong Bui Vu <hbui-vu@lab1r1s9.42abudhabi.ae>
2023-08-14 17:30:43 +0400

    wip 8.14.23

--- sources/resize.c
+++ sources/resize.c
@@ -7,6 +7,31 @@
 1. find which slice of wall the ray is hitting (check for x column) 
 2. find the wall height (pixel height)
 3. divide wall height / upg to find scale of the wall
-4. 
+4. Throw into bilinear interpolate function
 */
+
+int main(void)
+{
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "../textures/east-_1__.xpm";
+	int		img_width;
+	int		img_height;
+
+	img_width = 0;
+	img_height = 0;
+	img = NULL;
+	mlx = NULL;
+
+	mlx = mlx_init();
+	if (!mlx)
+		printf("error\n");
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
+	if (!img)
+		printf("error img\n");
+	else
+		printf("imgw: %i imgh: %i\n", img_width, img_height);
+}
+
+
 
