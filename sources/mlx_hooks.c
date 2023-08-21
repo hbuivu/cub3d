84354bc5971c19ecbbaf7a719981ca848e5a6264
Huong Bui Vu <hbui-vu@lab1r6s8.42abudhabi.ae>
2023-08-21 18:23:36 +0400

    more movement

--- sources/mlx_hooks.c
+++ sources/mlx_hooks.c
@@ -6,7 +6,7 @@
 /*   By: hbui-vu <hbui-vu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 20:51:30 by zsyyida           #+#    #+#             */
-/*   Updated: 2023/08/21 17:49:52 by hbui-vu          ###   ########.fr       */
+/*   Updated: 2023/08/21 18:23:19 by hbui-vu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 */
 
 //do we need to destroy all images before closing
+//need to implement wall collision
+//issue with rotation and moving forward, check that angles are being added correctly
 
 int	ft_close(t_main *main)
 {
@@ -26,6 +28,38 @@ int	ft_close(t_main *main)
 	exit(0);
 }
 
+int	move(int key_code, t_main *main)
+{
+	main->calc->angle = main->calc->pdir - (main->calc->fov / 2);
+	if (main->calc->angle < 0)
+		main->calc->angle += main->calc->rad_360;
+	else if (main->calc->angle > main->calc->rad_360 || ch_num(main->calc->angle, main->calc->rad_360))
+		main->calc->angle -= main->calc->rad_360;
+	main->calc->tan_angle = tan(main->calc->angle);
+	if (key_code == W_KEY)
+	{
+		main->calc->px += fabs(cos(main->calc->pdir)) * main->calc->stepx * WALK;
+		main->calc->py += fabs(sin(main->calc->pdir)) * main->calc->stepy * WALK;
+	}
+	else if (key_code == S_KEY)
+	{
+		main->calc->px += fabs(cos(main->calc->pdir)) * main->calc->stepx * -1 * WALK;
+		main->calc->py += fabs(sin(main->calc->pdir)) * main->calc->stepy * -1 * WALK;
+	}
+	else if (key_code == A_KEY)
+	{
+		main->calc->px += fabs(sin(main->calc->pdir)) * main->calc->stepy * -1 * WALK;
+		main->calc->py += fabs(cos(main->calc->pdir)) * main->calc->stepx * WALK;
+	}
+	else if (key_code == D_KEY)
+	{
+		main->calc->px += fabs(sin(main->calc->pdir)) * main->calc->stepy * WALK;
+		main->calc->py += fabs(cos(main->calc->pdir)) * main->calc->stepx * -1 * WALK;
+	}
+	return (0);
+}
+
+//still need to implement wall collision
 int	key_press(int key_code, t_main *main)
 {
 	if (key_code == ESC)
@@ -50,51 +84,10 @@ int	key_press(int key_code, t_main *main)
 			main->calc->angle += main->calc->rad_360;
 		calc_step(main);
 	}
-	else if(key_code == W_KEY)
-	{
-		main->calc->angle = main->calc->pdir - (main->calc->fov / 2);
-		if (main->calc->angle < 0)
-			main->calc->angle += main->calc->rad_360;
-		else if (main->calc->angle > main->calc->rad_360 || ch_num(main->calc->angle, main->calc->rad_360))
-			main->calc->angle -= main->calc->rad_360;
-		main->calc->tan_angle = tan(main->calc->angle);
-		main->calc->px += fabs(cos(main->calc->pdir)) * main->calc->stepx * WALK;
-		main->calc->py += fabs(sin(main->calc->pdir)) * main->calc->stepy * WALK;
-	}
-	
+	else if (key_code == W_KEY || key_code == S_KEY || key_code == A_KEY || key_code == D_KEY)
+		move(key_code, main);
 	else
 		return (0);
-	//check for every step if it takes you into a wall
-	// else if (key_code == W_KEY) //NORTH
-	// {
-	// 	for (int i; i < 5; i++)
-	// 	{
-	// 		if (ch_num(fmod(main->calc->px, 64.0), 0))
-	// 			if (!check_coord(COL, main))
-	// 				break;
-	// 		else if (ch_num(fmod(main->calc->py, 64.0), 0))
-	// 			if (check_coord(ROW, main))
-	// 				break;
-	// 		main->calc->px += fabs(cos(main->calc->pdir)) * main->calc->stepx;
-	// 		main->calc->py += fabs(sin(main->calc->pdir)) * main->calc->stepy;			
-	// 	}
-		
-	// }
-	// else if (key_code == S_KEY) //SOUTH
-	// {
-	// 	main->calc->px += cos(main->calc->pdir) * main->calc->stepx * -1;
-	// 	main->calc->py += sin(main->calc->pdir) * main->calc->stepy * -1;
-	// }
-	// else if (key_code == D_KEY) //EAST
-	// {
-	// 	main->calc->px += sin(main->calc->pdir) * main->calc->stepx;
-	// 	main->calc->py += cos(main->calc->pdir) * main->calc->stepy * -1;
-	// }
-	// else if (key_code == A_KEY) //WEST
-	// {
-	// 	main->calc->px += sin(main->calc->pdir) * main->calc->stepx * -1;
-	// 	main->calc->py += cos(main->calc->pdir) * main->calc->stepy;
-	// }
 	raycast(main);
     return (0);
 }
