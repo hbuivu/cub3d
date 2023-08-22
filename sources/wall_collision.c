2aed2c70c17aebbaa1e48007dade3b0996281ece
Huong Bui-Vu <huong.buivu@gmail.com>
2023-08-23 02:09:22 +0400

    wall collision need to test

--- sources/wall_collision.c
+++ sources/wall_collision.c
@@ -1,6 +1,15 @@
 #include "../include/cub3D.h"
 
-double	cast_hv_line(char line_dir, t_calc *c, t_main *main)
+/* 
+1. cast a ray in pdir to find player distance to the nearest wall
+2. find player distance traveled with one step
+3. player should not be able to move right up against the wall. there should be a small (4 pixel) buffer
+3. compare the two distances with buffer
+	-if player-to-wall-dist - buff >= player-walk-dist => okay
+	-if player-to-wall-dist - buff < player-walk-dist =>
+		walk only the amount of steps possible up until wall-buff*/
+
+double	cast_pdir_hv_ray(char line_dir, t_calc *c, t_main *main)
 {
 	if (line_dir == 'h')
 	{
@@ -27,7 +36,7 @@ double	cast_hv_line(char line_dir, t_calc *c, t_main *main)
 	return (fabs(c->row_int - c->py));
 }
 
-void	calc_intercepts(t_calc *c, t_main *main)
+double	cast_pdir_ray(t_calc *c, t_main *main)
 {
 	c->deltay = fabs(c->upg * c->tan_pdir);
 	c->deltax = fabs(c->upg / c->tan_pdir);
@@ -51,42 +60,12 @@ void	calc_intercepts(t_calc *c, t_main *main)
 		c->row_int += c->pdir_stepy * c->upg;
 		c->row_intx += c->pdir_stepx * c->deltax;
 	}
-}
-
-double	cast_line(int x, t_calc *c, t_main *main)
-{
-	calc_intercepts(c, main);
 	c->dist_col = fabs((c->px - c->col_int) / cos(c->angle));
 	c->dist_row = fabs((c->px - c->row_intx) / cos(c->angle));
 	if (c->dist_col <= c->dist_row)
 		return (c->dist_col);
 	else
 		return (c->dist_row);
-}
-
-void	check_collision(int key_code, t_main *main)
-{
-	t_calc	*c;
-	double	wall_dist;
-
-	c = c;
-	if (ch_num(c->pdir, 0) || ch_num(c->pdir, M_PI))
-		wall_dist = cast_hv_line('h', c, main);
-	else if (ch_num(c->pdir, c->rad_90) || ch_num(c->pdir, c->rad_270))
-		wall_dist = cast_hv_line('v', c, main);
-	else
-		wall_dist = cast_line(c, main);
-	move_dist = calc_move_dist(key_code, main);
-	//if we are walking check the walking dist
-	if (c->walk_dist < wall_dist)
-		//walk
-	else
-		//allow movement only up until 4 pixels before wall;
-	//if we are running check the run dist
-	if (c->run_dist < wall_dist)
-		//run
-	else
-		//allow moement only up until 4 pixels before wall
 }
 
 void	calc_move_dist(int key_code, t_main *main)
@@ -103,7 +82,7 @@ void	calc_move_dist(int key_code, t_main *main)
 		c->walk_dist = c->x_walk ;
 		c->run_dist = c->x_run * fabs(cos(c->pdir));
 	}
-	else if (key_code == A_KEY || D_KEY)
+	else if (key_code == A_KEY || key_code == D_KEY)
 	{
 		c->x_walk = fabs(sin(c->pdir)) * WALK;
 		c->y_walk = fabs(cos(c->pdir)) * WALK;
@@ -114,6 +93,48 @@ void	calc_move_dist(int key_code, t_main *main)
 	}
 }
 
+void	check_collision(int key_code, t_main *main)
+{
+	t_calc	*c;
+	double	wall_dist;
+
+	c = main->cacl;
+	if (ch_num(c->pdir, 0) || ch_num(c->pdir, M_PI))
+		wall_dist = cast_hv_line('h', c, main);
+	else if (ch_num(c->pdir, c->rad_90) || ch_num(c->pdir, c->rad_270))
+		wall_dist = cast_hv_line('v', c, main);
+	else
+		wall_dist = cast_line(c, main);
+	calc_move_dist(key_code, main);
+	if (c->walk_dist > wall_dist - 4)
+	{
+		c->walk_dist =  wall_dist - 4;
+		if (key_code == W_KEY || key_code == S_KEY)
+		{
+			c->x_walk = fabs(cos(c->pdir)) * c->walk_dist;
+			c->y_walk = fabs(sin(c->pdir)) * c->walk_dist;
+		}
+		else if (key_code == A_KEY || key_code == D_KEY)
+		{
+			c->x_walk = fabs(sin(c->pdir)) * c->walk_dist;
+			c->y_walk = fabs(cos(c->pdir)) * c->walk_dist;
+		}
+	}
+	if (c->run_dist > wall_dist - 4)
+	{
+		c->run_dist = wall_dist - 4;
+		if (key_code == W_KEY || key_code == S_KEY)
+		{
+			c->x_walk = fabs(cos(c->pdir)) * c->run_dist;
+			c->y_walk = fabs(sin(c->pdir)) * c->run_dist;
+		}
+		else if (key_code == A_KEY || key_code == D_KEY)
+		{
+			c->x_walk = fabs(sin(c->pdir)) * c->run_dist;
+			c->y_walk = fabs(cos(c->pdir)) * c->run_dist;
+		}
+	}
+}
 
 //when pdir changes, reset angle calculations 
 void	reset_pdir(t_main *main)
@@ -130,6 +151,31 @@ void	reset_pdir(t_main *main)
 	calc_step(main);
 }
 
+int	move(int key_code, t_main *main)
+{	
+	reset_pdir(main);
+	check_collision(key_code, main);
+	if (key_code == W_KEY)
+	{
+		c->px += c->x_walk * c->pdir_stepx;
+		c->py += c->y_walk * c->pdir_stepy;
+	}
+	else if (key_code == S_KEY)
+	{
+		c->px += c->x_walk * c->pdir_stepx * -1;
+		c->py += c->y_walk * c->pdir_stepy * -1;
+	}
+	else if (key_code == A_KEY)
+	{
+		c->px += c->x_walk * c->pdir_stepx * -1;
+		c->py += c->y_walk * c->pdir_stepy;
+	}
+	else if (key_code == D_KEY)
+	{
+		c->px += c->x_walk * c->pdir_stepx;
+		c->py += c->y_walk * c->pdir_stepy * -1;
+	}
+}
 // void	calc_walk(int key_code, t_main *main)
 // {
 // 	t_calc *c;
